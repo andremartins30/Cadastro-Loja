ae2f8fd759da0fb0896ce79dfaba866611788a8d
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2022-09-27 20:51:25 -0400

    Connect mongodb Atlas

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Cadastro-Loja')
+    await mongoose.connect('mongodb+srv://andremartins30:Kabana12@cluster0.7qd9cly.mongodb.net/?retryWrites=true&w=majority')
     console.log('Conectou ao MongoDB com o Mongoose!')
 
 }
